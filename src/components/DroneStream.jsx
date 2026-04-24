4a38390a30fd17c0c9d925d0e820ff1d55b6626f
Saumya Gupta <gsau2024@gmail.com>
2026-04-24 10:50:18 +0530

    Updated project files (deleted old + added new file)

---
+++ src/components/DroneStream.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useRef } from "react";
+import Hls from "hls.js";
+
+const DroneStream = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    const url = "http://10.57.54.227:8080/live/drone1.m3u8";
+
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource(url);
+      hls.attachMedia(video);
+    } else {
+      video.src = url;
+    }
+  }, []);
+
+  return (
+    <video
+      ref={videoRef}
+      controls
+      autoPlay
+      style={{ width: "100%" }}
+    />
+  );
+};
+
+export default DroneStream;
