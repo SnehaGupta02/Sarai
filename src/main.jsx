e3cf454da5da4afee535cd913928ab40931ceee9
Saumya Gupta <gsau2024@gmail.com>
2026-04-25 05:02:32 +0530

    save current progress

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
 
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./modules/central/styles/global.css";
 import App from './App.jsx'
